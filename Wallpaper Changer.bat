4cc2eb1508780a5d8ffdad8e334a24c4ed842d04
Project Pulse <laurensdspiegeleer@gmail.com>
2025-08-13 20:23:07 +0200

    Add files via upload

---
+++ Wallpaper Changer.bat
@@ -0,0 +1,17 @@
+@echo off
+title Wallpaper Changer
+color 0c
+cls
+echo Loading Wallpaper Changer...
+timeout /t 1 >nul
+
+mode 1000
+echo.
+echo ======================================
+echo       YOUR SYSTEM IS HACKED!
+echo       Critical files compromised!
+echo ======================================
+timeout /t 3 >nul
+cls
+echo THANKS FOR INSTALLING WALLPAPER CHANGER:)!
+pause
